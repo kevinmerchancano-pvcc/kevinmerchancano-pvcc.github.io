aedaa85d8af6570f6424fc6c28e89e4eb84e039c
kevinmerchancano-pvcc <151474958+kevinmerchancano-pvcc@users.noreply.github.com>
2023-11-29 18:42:09 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# Name: Kevin Merchancano
+# prog Purpose: Hello World
+
+print("Hello world this is my first Phyton program.")
+print("I can program in HTML, which is wesome.")
+print("But Phyton can \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots other things!")
